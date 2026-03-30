91105cdd9f48c9f024a820c0f88a5d02148067d1
aymanabdulhai33-hub <aymanabdulhai33@gmail.com>
2026-03-30 15:19:01 +0300

    config file

--- src/onedayConfig/OneDayApiConfig.js
+++ src/onedayConfig/OneDayApiConfig.js
@@ -1,12 +1,14 @@
-const API_INFO = {
-  mainUrl: "https://api.1daycloud.com/v3/",
-  Authorization:
-    "dev_token_49xb4g53zv69jxgt670wp167ug5ulkxk849qq1wpg0ywkqpgdyffb5xpdkvutitqf8yd06f2f432yhrecaaj5zsbr6e9j31k018iwyc3nmtsa1zflqft4e",
-  TemplatesAppAuthorization:
-    "dev_token_2mx5cm7s7d2p2s8ugu40vabr6bv7g88gt7of8zkaa4vthy41a8iqy24e9h0i72z0ymu2ed0mp6ulr9ahe9fwhogcy67mq2xspdl27cgkg751zlbzk3pakx",
-  appId: 1547,
-  templatesSourceAppId: 2421,
-};
-export const ONEDAY_CONFIG = () => {
-  return API_INFO;
-};
+
+          const API_INFO = {
+              mainUrl: "https://api.1daycloud.com/v3/",
+              Authorization:
+                "dev_token_6r4evsdfpegz5vwac3e8yhk9aqn23v25l814ek72o9br317cn61ws4v74si2epinjx0l0qn32kw3k7krpiohu5ps65evyqmg6a0uwz9jrmqlzg98zwpd5r",
+              appId: 4183,
+              TemplatesAppAuthorization:
+              "dev_token_2mx5cm7s7d2p2s8ugu40vabr6bv7g88gt7of8zkaa4vthy41a8iqy24e9h0i72z0ymu2ed0mp6ulr9ahe9fwhogcy67mq2xspdl27cgkg751zlbzk3pakx",
+              templatesSourceAppId: 2421,
+            };
+            export const ONEDAY_CONFIG = () => {
+              return API_INFO;
+            };
+        
